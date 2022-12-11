932854aa1690024871a41b8641ba766bb28008b5
Blackishjay <juniior4x@gmail.com>
2022-12-11 14:07:20 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is Zero\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
