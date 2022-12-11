43695718b5f36957117888c6955f3abf12e29d9a
Blackishjay <juniior4x@gmail.com>
2022-12-10 18:04:58 -0800

    Cis fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/*
+ * main - print a line using puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
