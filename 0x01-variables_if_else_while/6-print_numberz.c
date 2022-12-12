e08bc3b267ca2d04cc007808f5a55dea3abda74c
Blackishjay <juniior4x@gmail.com>
2022-12-11 16:14:23 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints out numbers from 0 to 9 without using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
