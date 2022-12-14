7035b7a200cffc97ec4e6e9b02026016593a50c3
Blackishjay <juniior4x@gmail.com>
2022-12-14 15:26:22 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet- prints the alphabet a-z to the screen
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
