cd2276a6d0db30d1dcd964779523bf1922ccafa7
Blackishjay <juniior4x@gmail.com>
2023-01-02 16:30:40 -0800

    task4

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *set_string- it sets the value of pointer to a char
+ *@s: source addresss
+ *@to: target adddress
+ *
+ *Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
